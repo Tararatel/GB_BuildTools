f2a82c24a4c9f8e294071d8ac3c3ad0e82064c4e
Tararatel <caratel18@mail.ru>
2021-07-03 17:02:03 +0300

    Lesson 2

---
+++ Lesson_2/webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { resolve } = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = {
+    entry: resolve(__dirname, 'js', 'main.js'),
+    output: {
+        filename: 'main.[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: resolve(__dirname, 'index.html') }),
+        new BundleAnalyzerPlugin
+    ]   
+};
+    
